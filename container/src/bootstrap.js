947dfe25dd3dbd3fcbe90b032b33e654476fb944
Marcos Silva <marcos.201314@hotmail.com>
2024-07-13 15:36:40 -0300

    ## The end of the section 3 - I learned how to change a simple id name could occurs big errors - The importance of creating a export function to avoid conflicts

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,5 +1,8 @@
-import 'products/ProductsIndex';
-import 'cart/CartShow';
+import { mount as productsMount } from 'products/ProductsIndex';
+import { mount as cartMount } from 'cart/CartShow';
 
 
 console.log('Container!');
+
+cartMount(document.querySelector('#my-cart'));
+productsMount(document.querySelector('#my-products'));
